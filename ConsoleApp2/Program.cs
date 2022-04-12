b0fee9fd582f069307067417b79ffa08fe1b4bf0
Mollaev <wsr5@K218PCREZERV>
2022-04-12 10:04:49 +0300

    Задание 6

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -14,20 +14,24 @@ namespace ConsoleApp2
             //Создать не явно типизированную переменную и присвоить ей любое значение
             var c = 32;
             //Вывести на консоль все переменные в строчку 1 методом
-            Console.WriteLine(a);
+            Console.WriteLine($"{a} {b} {c}") ;
             //5. Возвести в квадрат целочисленную переменную и вывести результат на консоль
             a = 1 * 1;
             Console.WriteLine(a);
+            //
+            Console.WriteLine(12 % 4); 
+            
             //Принять с консоли 3 числа и перемножить их, результат вывести на консоль
             Console.WriteLine("Введите переменные a, b, c");
             int f = Convert.ToInt32(Console.ReadLine());
             int g = Convert.ToInt32(Console.ReadLine());
+            
             int e = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(f * g * e);
             Console.ReadKey();
 
             // Принять с консоли 2 числа и найти большее из них
-            Console.WriteLine("Введите два числа");
+            Console.WriteLine("Введите два числа"); 
             int w = Convert.ToInt32(Console.ReadLine());
             int d = Convert.ToInt32(Console.ReadLine());
             if (w > d)
